cd3fd7f7c38c1ea5315e7b761d05b22f6b5557db
maazg222 <nikomaaz387@gmail.com>
2026-03-16 23:34:07 +0500

    fix: trim firebase private key

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -14,7 +14,7 @@ if (process.env.FIREBASE_PROJECT_ID) {
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
     private_key: process.env.FIREBASE_PRIVATE_KEY 
-      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"(.*)"$/, '$1') 
+      ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"(.*)"$/, '$1').trim() 
       : undefined,
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
